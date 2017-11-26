b1786559591ddfd643682b12b3e8ef93ae073f4f
Lohan Bodevan <lohan.bodevan@gmail.com>
2017-11-26 01:37:48 -0200

    Remove not used libraries

--- dokku_dashboard/__init__.py
+++ dokku_dashboard/__init__.py
@@ -4,10 +4,4 @@ from flask import Flask
 
 
 app = Flask(__name__)
-
-dotenv_path = os.path.join(os.getcwd(), '.env')
-if os.path.isfile(dotenv_path):
-    from dotenv import load_dotenv
-    load_dotenv(dotenv_path)
-
 app.debug = os.environ.get('DEBUG', False)
